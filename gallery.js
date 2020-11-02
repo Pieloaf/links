8942cf02902505dd205906d8c0b0081b53297c1b
unknown <piercelowe1@gmail.com>
2020-11-02 12:35:29 +0000

    added yt embed and insta and changed file structure

---
+++ gallery.js
@@ -0,0 +1,6 @@
+var gallerDiv = document.getElementById("images");
+var testimage = document.createElement("img");
+
+testimage.className = "image";
+testimage.src = "./thumbs/iceland/1.png";
+gallerDiv.appendChild(testimage);
---
+++ openimage.js
@@ -0,0 +1,10 @@
+var images = document.getElementsByClassName("image");
+var openImage = function () {
+  var source = this.getAttribute("src");
+  source = source.replace("thumbs/", "Photos/");
+  var win = window.open(source, "_blank");
+  win.focus();
+};
+for (var i = 0; i < images.length; i++) {
+  images[i].addEventListener("click", openImage, false);
+}
