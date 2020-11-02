6d8cfbe3f721794e6837a32973ceafbd6ced5160
unknown <piercelowe1@gmail.com>
2020-11-02 16:32:52 +0000

    basic responsiveness

--- gallery.js
+++
@@ -1,6 +0,0 @@
-var gallerDiv = document.getElementById("images");
-var testimage = document.createElement("img");
-
-testimage.className = "image";
-testimage.src = "./thumbs/iceland/1.png";
-gallerDiv.appendChild(testimage);
